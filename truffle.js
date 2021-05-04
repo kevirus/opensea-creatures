5050e0ca763a1db311738f2346a45d88349b8f91
Kachnov Kirill <72107396+kevirus@users.noreply.github.com>
2021-05-04 19:10:25 +0300

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 const MNEMONIC = process.env.MNEMONIC;
 const NODE_API_KEY = process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
